c7abfd4fc8d5743366de8c268fd4b1bb277ad97d
Seyi Oyemade <seyioyemade918@gmail.com>
2023-06-15 14:26:01 +0100

    Fix linter error

--- index.rb
+++ index.rb
@@ -1,49 +1,45 @@
-def decode_char(morse_code)
-  morse_code_dict = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
-  }
+$morse_code_dict = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
 
-  morse_code_dict.each { 
-    |key,value| 
+def decode_char(morse_code)
+  $morse_code_dict.each { |key, value|
     if morse_code == key
       return value
     end
   }
 
   return ''
-
 end
 
 # puts decode_char '-.-'
 
-
 def decode_word(morse_code)
-
   character = ''
   word = ''
 
@@ -58,21 +54,19 @@ def decode_word(morse_code)
   end
 
   dec_char = decode_char (character)
-  word += dec_char 
+  word += dec_char
 
   return word
 end
 
 # puts decode_word "-- -.--"
 
-
-
 def decode(morse_code)
   word = ''
   message = ''
 
   morse_code.each_char.with_index do |char, index|
-    if char == ' ' && morse_code[index+1] == ' ' && morse_code[index+2] == ' '
+    if char == ' ' && morse_code[index + 1] == ' ' && morse_code[index + 2] == ' '
       dec_word = decode_word(word)
       message += dec_word
       message += ' '
@@ -89,9 +83,3 @@ def decode(morse_code)
 end
 
 puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-
-
- 
-
-
-
